207204b2cd7b670a8836490a389ba7f48a01f4a1
feanor07 <alptugd@gmail.com>
2017-03-07 14:51:32 +0300

    finished cell-customization part

--- app/components/custom-table.js
+++ app/components/custom-table.js
@@ -2,14 +2,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  ages:Ember.computed.mapBy('selectedRows','age'),
-  totalAges:Ember.computed.sum('ages'),
-  tableClassNames:'table table-striped table-bordered table-hover table-responsive table-condensed',
-
-  actions:{
-    selectionChanged:function(selectedRows){
-      this.set('selectedRows', selectedRows);
-    }
-  }
+  currentYear: new Date().getFullYear(),
+  tableClassNames:'table table-striped table-bordered table-hover table-responsive table-condensed'
 });
 // END-SNIPPET
