372a90bcd98960ae472283746862f25b27afc86f
feanor07 <alptugd@gmail.com>
2017-03-08 15:56:12 +0300

    done with filtering

--- app/components/custom-filtered-table.js
+++ app/components/custom-filtered-table.js
@@ -1,54 +1,20 @@
 // BEGIN-SNIPPET custom-filtered-table
 import Ember from "ember";
-import data from "../data";
 const {isEmpty, get} = Ember;
-
-function nameFilterer(item, filterField) {
-  return get(item, 'name').toLowerCase().indexOf(get(filterField, 'filter').toLowerCase()) >= 0;
-}
-
-function teamFilterer(item, filterField) {
-  let selectedTeams = get(filterField, 'filter');
-
-  if (selectedTeams.length === 0) {
-    return true;
-  }
-
-  for (const selectedTeam of selectedTeams) {
-    if (get(item, 'team') === get(selectedTeam, 'shortName')) {
-
-      return true;
-    }
-  }
-
-  return false;
-}
 
 export default Ember.Component.extend({
   tableClassNames:'table table-striped table-bordered table-hover table-responsive table-condensed',
-  teams: data.teams,
+  positions: ['Guard', 'Forward', 'Center'],
 
-  customFilterer(data, filterFields) {
-    if (isEmpty(filterFields)) {
-      return data;
+  filteredData: Ember.computed('data', 'selectedPosition', function () {
+    if (isEmpty(this.get('selectedPosition'))) {
+      return this.get('data');
     }
 
-    return data.filter((item)=>{
-      for (const filterField of filterFields) {
-        if (get(filterField, 'fieldName') === 'name') {
-          if (nameFilterer(item, filterField) === false) {
-            return false;
-          }
-        } else if (get(filterField, 'fieldName') === 'team') {
-          if (teamFilterer(item, filterField) === false) {
-            return false;
-          }
-        }
-      }
-
-      return true;
+    return this.get('data').filter((item)=>{
+      return get(item, 'position') === this.get('selectedPosition');
     });
-  },
+  }),
 
   actions:{
     selectionChanged: function(selectedRows){
