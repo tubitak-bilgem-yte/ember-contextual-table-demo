33d9e7a4cc11543b52a44e925938edf6e4df096b
feanor07 <alptugd@gmail.com>
2017-03-10 15:45:30 +0300

    data-filterer api done

--- app/components/filterable-column-options.js
+++ app/components/filterable-column-options.js
@@ -4,9 +4,9 @@ export default Ember.Component.extend({
   data: [
     {option: 'filterinformationupdated', type: 'closure-action', description: 'Closure action to be fired whenever filter ' +
     'value changes. The handling action definitions should expect the following parameters in order. <ul><li><b>fieldName</b> : ' +
-    'the name (identifier) of the field (property) the filtering is being applied</li><li><b>filterValue</b> : The value ' +
-    'of the filter to be applied to for the specified field</li></ul> This property is typically expected to be assigned ' +
-    '<code>onfilterfieldupdated</code> yielded from <code>data-filterer</code> when used together.'},
+    'string type value that is the name of the field (property) the filtering is being applied</li><li><b>filterValue</b> ' +
+    'object type field that is the value to be applied as filter to the specified field</li></ul> This property is typically expected ' +
+    'to be assigned <code>onfilterfieldupdated</code> yielded from <code>data-filterer</code> when used together.'},
     {option: 'placeHolder', type: 'string', description: 'The value to to display as prompt text for the input component ' +
     'that is displayed when <code>filterableColumn</code> is used in basic form as opposed to block form.'},
     {option: 'propertyName', type: 'string', description: 'The field name that determines the name of the property filtering ' +
--- app/components/sortable-column-options.js
+++ app/components/sortable-column-options.js
@@ -8,8 +8,8 @@ export default Ember.Component.extend({
     'is to be applied for.'},
     {option: 'sortinformationupdated', type: 'closure-action', description: 'Closure action to be fired whenever sorting ' +
     'information for a specified property changes. The handling action definitions should expect the following parameters ' +
-    'in order. <ul><li><b>fieldName</b> : the name (identifier) of the field (property) the sorting is being applied</li>' +
-    '<li><b>isAscending</b> : A <code>boolean</code> value to determine order of sorting. <code>true</code> should be treated ' +
+    'in order. <ul><li><b>fieldName</b> : string type value, that is the name of the field (property) the sorting is being applied</li>' +
+    '<li><b>isAscending</b> : a <code>boolean</code> value to determine order of sorting. <code>true</code> should be treated ' +
     'as ascending; false should be treated as descending; and undefined should be treated as no sorting.</li></ul> This ' +
     'property is typically expected to be assigned <code>onsortfieldupdated</code> yielded from <code>data-sorter</code> ' +
     'when used together.'}
