1ef95d1286f61f07e2b624a88d97dcf7492868e3
feanor07 <alptugd@gmail.com>
2017-03-10 15:00:24 +0300

    customHeader & customFooter api reference done

---
+++ app/components/custom-header-attributes.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  data: [
+    {option: 'deselected', type: 'closure-action', description: 'The action to fire to trigger deselection of all rows of ' +
+    'table. Code snippets to fire this action are expected to call without any parameters.'},
+    {option: 'isSelected', type: 'boolean', description: 'The flag to show whether all the rows of the table are selected ' +
+    'or not.'},
+    {option: 'selected', type: 'closure-action', description: 'The action to fire to trigger selection of all rows of ' +
+    'table. Code snippets to fire this action are expected to call without any parameters.'},
+  ]
+});
