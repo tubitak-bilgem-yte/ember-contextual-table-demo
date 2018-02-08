49a2f126b6d9f1f640fa475573b0a80c8289168a
feanor07 <alptugd@gmail.com>
2018-02-08 10:49:31 +0300

    correcting links in menu

--- app/components/navigation-menu-item.js
+++ app/components/navigation-menu-item.js
@@ -3,6 +3,7 @@ import { computed } from '@ember/object';
 
 export default Component.extend({
   tagName:'',
+
   linkClass: computed('isActive', function () {
     return this.get('isActive') ? 'active-link' : '';
   })
