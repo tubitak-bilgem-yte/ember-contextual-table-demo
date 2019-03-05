14f88719840d07413fa0a58fb4cb1ea775bc1e80
ahmetemrekilinc <aekilinc@gmail.com>
2019-03-05 15:28:23 +0300

    3.3.0 ember-cli is upgraded to 3.8.1

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,17 +23,11 @@ module.exports = {
         '.eslintrc.js',
         '.template-lintrc.js',
         'ember-cli-build.js',
-        'index.js',
         'testem.js',
         'blueprints/*/index.js',
         'config/**/*.js',
-        'tests/dummy/config/**/*.js'
-      ],
-      excludedFiles: [
-        'addon/**',
-        'addon-test-support/**',
-        'app/**',
-        'tests/dummy/app/**'
+        'lib/*/index.js',
+        'server/**/*.js'
       ],
       parserOptions: {
         sourceType: 'script',
